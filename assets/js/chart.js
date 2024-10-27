371d6cd04298345b8f065c856b7c13d78e631ef0
Zilfs <rheinlandcast@gmail.com>
2024-10-28 01:29:47 +0700

    refactor(footer): simplified footer content | feat(page): remove unnecessary content sections

---
+++ assets/js/chart.js
@@ -0,0 +1,16 @@
+const ctx = document.getElementById('stackedChart')
+
+const stackedBar = new Chart(ctx, {
+    type: 'bar',
+    data: data,
+    options: {
+        scales: {
+            x: {
+                stacked: true
+            },
+            y: {
+                stacked: true
+            }
+        }
+    }
+});
